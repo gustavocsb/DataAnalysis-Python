75794d84fc13e196b6e95305fc408ed563a44d50
gustavocsb <gcsb03@hotmail.com>
2023-09-18 08:07:58 -0300

    Adicionado código do bubble_sort.py
    Bubble sort é um problema clássico de lógica de programação, é uma ordenação por flutuação.

--- 01.Introdutorio/06.bubble_sort.py
+++ 01.Introdutorio/06.bubble_sort.py
@@ -1,4 +1,15 @@
 def bubble_sort(arr):
 
+    # definindo o tamanho do array
+    tamanho = len(arr)
+
+    for i in range(tamanho):
+        for j in range(0, tamanho-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+lista = [6,7,8,3,10,19,4,1,0,61,30,16,17,82,29,34,43,21,11,39,56,67,12]
+
+print(bubble_sort(lista))
 
