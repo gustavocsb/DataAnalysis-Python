01f248ecac67f916ccfe9f0a995baf8d1b502af3
gustavocsb <gcsb03@hotmail.com>
2024-05-28 09:43:39 -0300

    Adicionado arquivos.py
    Lendo arquivo usando funções do terminal do sistema (os), diretamente em python (with open 'r') e com numpy (loadtxt)
    
    Gerando um gráfico de dispersão com a função (plot) usando a biblioteca matplotlib

--- 02.Basico/02.arquivos.py
+++ 02.Basico/02.arquivos.py
@@ -13,6 +13,32 @@
 #                      |_|
 
 import os
-arquivo = os.path.join('dataset.csv')
+import numpy as np
+file_name = os.path.join('02.Basico', 'dataset.csv')
 
+# '!more dataset.csv' no Jupyter Notebook o prefixo '!' executa comandos do sistema operacional
+# Replicando em python:
+#arquivo = os.path.join('02.Basico', 'dataset.csv')
 
+#os.system('more ' + arquivo) # Comando 'more' em windows é um paginador que exibe o conteúdo do arquivo no console
+#os.system('type ' + arquivo) # Comando 'type' em windows exibe todo o conteúdo do arquivo no console
+
+# Abrindo o arquivo diretamente com python:
+# with open('02.Basico/dataset.csv', 'r') as file:
+#     for line in file:
+#         print(line)
+
+# Carregando o dataset dentro de um array com numpy
+file = np.loadtxt(file_name, delimiter=',', usecols=(0, 1, 2, 3), skiprows=1)
+print(file)
+print(f'Type: {type(file)}')
+
+# Carregando apenas duas variáveis (colunas) do dataset
+var1, var2 = np.loadtxt(file_name, delimiter=',', usecols=(0, 1), skiprows=1, unpack=True)
+
+# Gerando um plot a partir de um arquivo usando o matplotlib:
+
+import matplotlib.pyplot as plt # Importando a biblioteca matplotlib
+
+plt.plot(var1,var2, 'o', markersize=6,color='blue') # Criando um gráfico de dispersão com a função 'plot'
+plt.show() # Exibindo o gráfico com a função 'show'
